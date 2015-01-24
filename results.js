a3b5cc2770110dd805e77c40c647465a838ef77e
Nat Burgwyn <burgwyn@Nats-MacBook-Pro.local>
2015-01-24 15:33:46 -0500

    jquery iteration and clean up

--- results.js
+++ results.js
@@ -1,13 +1,11 @@
-var http = require('http'),
-	fs = require('fs');
-
+var http = require('http');
 var jsdom = require('jsdom');
 
 http.createServer(function(req, res){
 
-	 res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+	res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
   jsdom.env(
-    "http://www.premierleague.com/en-gb/matchday/results.html?paramComp_100=true&paramSeason=2013-2014&view=.dateSeason",
+    "http://www.premierleague.com/en-gb/matchday/results.html?paramComp_100=true&paramSeason=2014-2015&view=.dateSeason",
     ["http://code.jquery.com/jquery.js"],
     function (errors, window) {
 
@@ -17,7 +15,7 @@ http.createServer(function(req, res){
 
       	var strDate = window.$(this).find("th").text();
 
-      	window.$(this).children("tr").each(function(j){
+      	window.$(this).find("tr").each(function(j){
 
           if (window.$(this).find("th").text().length > 0)
             return;
@@ -34,7 +32,6 @@ http.createServer(function(req, res){
 
 						if (venue)
 							results.push({ date: date, score: score, homeTeam:homeTeam, awayTeam:awayTeam, homeScore: homeScore, awayScore: awayScore, venue: venue});
-
       		}
 
       	});
